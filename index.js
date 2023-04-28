64418fe35aa526262b490033341c74a022c25c8f
anis024544 <anis024544@gmail.com>
2023-04-28 16:28:34 +0600

    uri Changed

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.midyoic.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.midyoic.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://Japan-warehouse:E7Md9S08LBh43NPY@cluster0.midyoic.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
